6c49e6861df5783276b09913a08ec794d5eae0d3
Nikita Izygin <nr.izygin@tensor.ru>
2016-12-30 11:00:42 +0300

    index

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const bot = new Telegram(process.env.BOT_TOKEN);
 // const shit
 
 const ms = {
-  goodBoy:      'Молодец, что пришел. Можешь валить пыхать сейчас или через час. Мне похер.',
+  goodBoy:      'Молодец, что пришел. Запускай голосование или не запускай. Мне похер.',
   wtf:          'Че тебе нужно баран, я ж уже сказал! Сообщу, когда можно пыхнуть.',
   getOut:       'Вали из офиса, до связи.',
-  timeTo:       'Время попыхать пришло. Прямо сейчас.',
-  urgentTimeTo: 'ААААААА!!! БЫСТРЕЕ. ПИСОС. КУРИТЬ!'
+  timeTo:       'Время попыхать пришло. Запускай голосование.',
+  urgentTimeTo: 'ААААААА!!! БЫСТРЕЕ. ПИСОС. КУРИТЬ!',
+  yep:          '👍',
+  nope:         '👎',
+  getready:     'Собирайся',
+  skip:         'Пропускаешь покур',
+  pollover:     'Голосование прошло, запускай новое!'
 };
 
 const img   = 'v.jpg',
@@ -27,18 +32,34 @@ const img   = 'v.jpg',
       basta = 'basta.jpg',
       answers = { start: '🚬  Новый день', end: '🚭  Баста', now: '🔥 Срочный покур', how: 'Сколько осталось?'},
       resources = path.join(path.resolve(__dirname), 'resources'),
+      min  = 2000,
       hour = 3600000;
 
 let timers = {};
 
+let
+    concatStr = function concatStr() {
+      return [...arguments].join(' ');
+    },
+    getTeam = function getTeam(team) {
+      let begin = 'Состав покура: \n';
+      return begin + team.join('\n');
+    },
+    poll = function poll(from) {
+      return concatStr(from.first_name, from.last_name, 'предложил покур.');
+    },
+    calculate = function calculate(ctx) {
+      if (ctx.session.team.length > 0) {
+        ctx.replyWithPhoto({ source: path.join(resources, ur) }, { caption: getTeam(ctx.session.team) });
+      } else {
+        ctx.reply('Покур не состоится.');
+      }
+    };
 
-bot.use((ctx, next) => {
-  const start = new Date()
-  return next().then(() => {
-    const ms = new Date() - start
-    console.log('Response time %sms', ms)
-  })
-});
+
+
+
+bot.use(Telegram.memorySession());
 
 bot.command('start', (ctx) => {
   return ctx.replyWithPhoto({ source: path.join(resources, img) }, Telegram.Markup
@@ -52,8 +73,7 @@ bot.command('start', (ctx) => {
 
 bot.hears(answers.start, (ctx) => {
   let intervalId = setInterval(() => {
-    ctx.reply(ms.timeTo);
-    ctx.replyWithPhoto({ source: path.join(resources, sm) });
+    ctx.replyWithPhoto({ source: path.join(resources, sm) }, { caption: ms.timeTo});
   }, hour);
   timers[ctx.chat.id] = intervalId;
   return ctx.reply(ms.goodBoy,
@@ -70,40 +90,58 @@ bot.hears(answers.start, (ctx) => {
 bot.hears(answers.end, (ctx) => {
   clearInterval(timers[ctx.chat.id]);
   delete timers[ctx.chat.id];
-  ctx.replyWithPhoto({ source: path.join(resources, basta) });
-  return ctx.reply(ms.getOut,
-    Telegram.Markup
-      .keyboard([
-        [answers.start]
-      ])
-      .resize()
-      .extra()
-  )
+  return ctx.replyWithPhoto({ source: path.join(resources, basta) },
+  Telegram.Markup
+    .keyboard([
+      [answers.start]
+    ])
+    .resize()
+    .extra()
+  );
 });
 
 
 bot.hears(answers.now, (ctx) => {
-  clearInterval(timers[ctx.chat.id]);
-  delete timers[ctx.chat.id];
-  let intervalId = setInterval(() => {
-    ctx.reply(ms.timeTo);
-    ctx.replyWithPhoto({ source: path.join(resources, sm) });
-  }, hour);
-  timers[ctx.chat.id] = intervalId;
-  ctx.replyWithPhoto({ source: path.join(resources, ur) });
-  return ctx.reply(ms.urgentTimeTo);
+  // clearInterval(timers[ctx.chat.id]);
+  // delete timers[ctx.chat.id];
+  //
+  ctx.session.team = [];
+  ctx.session.on = true;
+  let intervalId = setTimeout(() => {
+    calculate(ctx);
+    ctx.session.on = false;
+    ctx.session.team = [];
+  }, min);
+  // timers[ctx.chat  .id] = intervalId;
+  return ctx.reply(poll(ctx.from),
+    Telegram.Markup.inlineKeyboard([
+        Telegram.Markup.callbackButton(ms.yep, ms.yep),
+        Telegram.Markup.callbackButton(ms.nope, ms.nope),
+    ]).extra()
+  );
 });
+
+bot.action(ms.yep, (ctx, next) => {
+  if (ctx.session.on) {
+    if (ctx.session.team) {
+      ctx.session.team.push([concatStr(ctx.from.first_name, ctx.from.last_name)]);
+    }
+    return ctx.answerCallbackQuery(ms.getready);
+  } else {
+    return ctx.answerCallbackQuery(ms.pollover);
+  }
+});
+
+bot.action(ms.nope, (ctx, next) => {
+  if (ctx.session.on) {
+    return ctx.answerCallbackQuery(ms.skip);
+  } else {
+    return ctx.answerCallbackQuery(ms.pollover);
+  }
+})
 
 bot.hears(answers.how, (ctx) => {
   return ctx.replyWithPhoto({ source: path.join(resources, w) });
-});
-
-
-bot.on('text', (ctx) => {
-  return Promise.all([
-    ctx.reply(ms.wtf),
-    ctx.replyWithPhoto({ source: path.join(resources, img) })
-  ])
 });
 
 bot.startPolling();
